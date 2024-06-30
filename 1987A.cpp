6e05d6673581e1cc6aa6f08882009e137edb35cb
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2024-06-30 23:23:22 +0530

    1987A Upload More RAM

---
+++ 1987A.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define f(x,n) for(ll i = x ; i<n ; i++) 
+#define all(x) x.begin(),x.end()  
+#define MOD 1000000007
+
+void solve() {
+    ll n, k; cin>>n>>k;
+    if(n == 1){
+        cout<<1<<endl;
+    }  
+    else{
+        ll num = ((n-1)*k)+1;
+        cout<<num<<endl;
+    } 
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        solve();
+    }
+
+    return 0;
+}
