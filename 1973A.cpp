f61df3ff921634c7181609acded16984d948af1e
Aman Singh <135147451+codewithaman07@users.noreply.github.com>
2024-07-01 15:13:05 +0530

    1973A -  Chess For Three

---
+++ 1973A.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define f(x,n) for(ll i = x ; i<n ; i++) 
+#define all(x) x.begin(),x.end()  
+#define MOD 1000000007
+
+void solve() {
+    ll a,b,c;
+    cin>>a>>b>>c;
+    ll sum = a+b+c;
+    if(sum&1){
+        cout<<-1<<endl;
+    }
+    else{
+        cout<<min(a+b,sum/2)<<endl;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        solve();
+    }
+
+    return 0;
+}
